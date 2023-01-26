70b8e79def40f9dd6ce94acbbd44cd15c4cddb69
이진아 <mnb2098@naver.com>
2023-01-26 16:04:21 +0900

    Routes 재설치

--- front/src/App.js
+++ front/src/App.js
@@ -6,10 +6,10 @@ import './App.css';
 import './swiper.css';
 import React,{useState, useEffect} from 'react';
 import Axios from 'axios';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 function App() {
     
-   
 
     // message 초기값을 ""으로 설정.
     const [message1, setMessage1] = useState("");
@@ -45,89 +45,89 @@ function App() {
             
     const [DaelimBox, setDaelimBox] = useState("");
     useEffect( () => {
-                    // fetch(url,options) : HTTP 요청 함수
-                    fetch('/user/DaelimBox')
-                    .then(response => response.text())
-                    .then(DaelimBox => {
-                        setDaelimBox(DaelimBox);
-                    });
-                    },[])
+            // fetch(url,options) : HTTP 요청 함수
+            fetch('/user/DaelimBox')
+            .then(response => response.text())
+            .then(DaelimBox => {
+                setDaelimBox(DaelimBox);
+             });
+            },[])
 
-return (
-    <div className="App">
-      <header id="header">
-        <div class="container">
-            <div class="row">
-                <div class="header clearfix">
-                    <h1>
-                        <a href="#">
-                            <h1>{DaelimBox}</h1>
-                        </a>    
-                    </h1>
-                    <nav class="nav">
-                        <ul class="clearfix">
-                            <li><a href="#">{message1}</a></li>
-                            <li><a href="#" target="_blank">{message2}</a></li>
-                            <li><a href="#">{message3}</a></li>
-                        </ul>
-                    </nav>    
-                </div>
-            </div>
-        </div>
-    </header>
-    <section id="banner">
-        <div class="banner_menu">
+    return (
+        <div className="App">
+        <header id="header">
             <div class="container">
                 <div class="row">
-                    <div class="bm_right">
-                        <ul>
-                            <li><a href="#">영화</a></li>
-                            <li><a href="#">예매</a></li>
-                        </ul>
-                        <div class="search">
-                          <input type="text" placeholder="검색어 입력"></input>
-                          <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"></img>
+                    <div class="header clearfix">
+                        <h1>
+                            <a href="#">
+                                <h1>{DaelimBox}</h1>
+                            </a>    
+                        </h1>
+                        <nav class="nav">
+                            <ul class="clearfix">
+                                <li><a href="#">{message1}</a></li>
+                                <li><a href="#" target="_blank">{message2}</a></li>
+                                <li><a href="#">{message3}</a></li>
+                            </ul>
+                        </nav>    
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section id="banner">
+            <div class="banner_menu">
+                <div class="container">
+                    <div class="row">
+                        <div class="bm_right">
+                            <ul>
+                                <li><a href="#">영화</a></li>
+                                <li><a href="#">예매</a></li>
+                            </ul>
+                            <div class="search">
+                            <input type="text" placeholder="검색어 입력"></input>
+                            <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"></img>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <div class="slider">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide ss1">
-                        <div class="container">
-                            <div class="row">
-                                <h2>코로나 <em>Covid-19</em></h2>
-                                <p>많은 사람들의 목숨을 앗아간 질병</p>
+        </section>
+        <div class="slider">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide ss1">
+                            <div class="container">
+                                <div class="row">
+                                    <h2>코로나 <em>Covid-19</em></h2>
+                                    <p>많은 사람들의 목숨을 앗아간 질병</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide ss2">
+                            <div class="container">
+                                <div class="row">
+                                    <h2>캡틴 마블 <em>Captain Marvel</em></h2>
+                                    <p>새로운 히어로, 어벤져스의 희망</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide ss3">
+                            <div class="container">
+                                <div class="row">
+                                    <h2>캡틴 마블 <em>Captain Marvel</em></h2>
+                                    <p>새로운 히어로, 어벤져스의 희망</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="swiper-slide ss2">
-                        <div class="container">
-                            <div class="row">
-                                <h2>캡틴 마블 <em>Captain Marvel</em></h2>
-                                <p>새로운 히어로, 어벤져스의 희망</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide ss3">
-                        <div class="container">
-                            <div class="row">
-                                <h2>캡틴 마블 <em>Captain Marvel</em></h2>
-                                <p>새로운 히어로, 어벤져스의 희망</p>
-                            </div>
-                        </div>
-                    </div>
+                    <div class="swiper-pagination"></div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
                 </div>
-                <div class="swiper-pagination"></div>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
