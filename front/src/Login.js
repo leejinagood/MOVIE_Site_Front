c479ab6b48ffdbb69bfc47d80d0e54b1e8e6b091
parksoyeoung <fndlsk2325@naver.com>
2023-02-15 05:05:25 +0900

    로그인 페이지 수정

--- front/src/Login.js
+++ front/src/Login.js
@@ -5,13 +5,17 @@ const Login = () => {
 	
     return (
 	<div className="Login">
-		<h2 className="title">로그인</h2>
+		<h2 className="title">Login</h2>
 			<form className="form" id="registerFrm" name="register-page" >
 					<input type="text" id="userId" name="userId"  placeholder="아이디" />
 					<input type="password" id="userPW" name="userPW" placeholder="비밀번호"/>
 					<input type="button" className="button medium primary"  id="joinBtn" value="로그인"/>
+					
       </form>
+	  
 	</div>
+
+	
 
    )
 };
